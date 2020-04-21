c4cb3519b91616df5d249f7f71872b20b3ce15fb
lzxm160 <lzxm160@sina.com>
2020-04-21 12:56:01 +0800

    x

--- core/vm/evm.go
+++ core/vm/evm.go
@@ -418,8 +418,9 @@ func (evm *EVM) create(caller ContractRef, codeAndHash *codeAndHash, gas uint64,
 
 	// check whether the max code size has been exceeded
 	maxCodeSizeExceeded := evm.ChainConfig().IsEIP158(evm.BlockNumber) && len(ret) > params.MaxCodeSize
-	fmt.Println(evm.ChainConfig().IsEIP158(evm.BlockNumber),len(ret),params.MaxCodeSize)
-	fmt.Println(maxCodeSizeExceeded)
+	fmt.Println("goethereum:::",evm.ChainConfig().IsEIP158(evm.BlockNumber),len(ret),params.MaxCodeSize)
+	fmt.Println("goethereum:maxCodeSizeExceeded::",maxCodeSizeExceeded)
+	fmt.Println("goethereum:err::",err)
 	// if the contract creation ran successfully and no errors were returned
 	// calculate the gas required to store the code. If the code could not
 	// be stored due to not enough gas set an error and let it be handled
@@ -437,6 +438,7 @@ func (evm *EVM) create(caller ContractRef, codeAndHash *codeAndHash, gas uint64,
 	// above we revert to the snapshot and consume any gas remaining. Additionally
 	// when we're in homestead this also counts for code storage gas errors.
 	if maxCodeSizeExceeded || (err != nil && (evm.ChainConfig().IsHomestead(evm.BlockNumber) || err != ErrCodeStoreOutOfGas)) {
+		fmt.Println("goethereum:maxCodeSizeExceeded::",maxCodeSizeExceeded)
 		evm.StateDB.RevertToSnapshot(snapshot)
 		if err != errExecutionReverted {
 			contract.UseGas(contract.Gas)
