21b2d8e0ae3f5153926974b96814ca9f571c71e4
lzxm160 <lzxm160@sina.com>
2020-04-21 16:31:12 +0800

    x

--- core/vm/evm.go
+++ core/vm/evm.go
@@ -460,6 +460,7 @@ func (evm *EVM) create(caller ContractRef, codeAndHash *codeAndHash, gas uint64,
 // Create creates a new contract using code as deployment code.
 func (evm *EVM) Create(caller ContractRef, code []byte, gas uint64, value *big.Int) (ret []byte, contractAddr common.Address, leftOverGas uint64, err error) {
 	contractAddr = crypto.CreateAddress(caller.Address(), evm.StateDB.GetNonce(caller.Address()))
+	fmt.Println("evm.go line 463,before call evm.create len(code):",len(code))
 	return evm.create(caller, &codeAndHash{code: code}, gas, value, contractAddr)
 }
 
