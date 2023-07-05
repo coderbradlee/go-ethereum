828c1fa78a0962b2d4d03cf3703dae1d9b389242
da <da@xx.com>
2023-07-05 16:38:34 +0800

    x

---
+++ start.sh
@@ -0,0 +1,5 @@
+#debug.traceTransaction('0x7ae446a7897c056023a8104d254237a8d97783a92900a7b0f7db668a9432f384', { tracer: 'opcounter' })
+#https://geth.ethereum.org/docs/developers/evm-tracing/custom-tracer
+#make geth
+#mv ./build/bin/geth ./
+./geth attach https://rpc.ankr.com/eth
